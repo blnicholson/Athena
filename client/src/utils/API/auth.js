e9548debbd0dc290b800c4ea7f11037c688e8bcc
blnicholson <blnicholson13@gmail.com>
2019-02-14 16:38:03 -0600

    api to API

--- client/src/utils/API/auth.js
+++ client/src/utils/API/auth.js
@@ -2,39 +2,39 @@ import axios from 'axios';
 
 export default {
 	register: (user) => {
-		return axios.post('/api/auth/register', user);
+		return axios.post('/API/auth/register', user);
 	},
 	login: (username, password) => {
-		return axios.post('/api/auth/login', { username, password });
+		return axios.post('/API/auth/login', { username, password });
 	},
 	getUsers: (params = {}) => {
-		return axios.get('/api/auth/users', { params });
+		return axios.get('/API/auth/users', { params });
 	},
 	findById: (params = {}) => {
-		return axios.get('/api/auth/user', { params });
+		return axios.get('/API/auth/user', { params });
 	},
 	update: user => {
-		return axios.put('/api/auth/user', user);
+		return axios.put('/API/auth/user', user);
 	},
 	addTrainingToExistingUsers: training => {
-		return axios.put('/api/auth/users', training);
+		return axios.put('/API/auth/users', training);
 	},
 	addTrainingInstances: (id, trainingInstancesArr) => {
-		return axios.put('/api/auth/addtrainings', { userID: id, trainingInstances: trainingInstancesArr });
+		return axios.put('/API/auth/addtrainings', { userID: id, trainingInstances: trainingInstancesArr });
 	},
 	addTrainingInstance: (id, trainingInstance) => {
 		return axios.put()
 	},
 	addTrainingHours: data => {
-		return axios.put('/api/auth/addhours', data);
+		return axios.put('/API/auth/addhours', data);
 	},
 	search: (organizationID, queryString) => {
 		const query = { organizationID: organizationID, queryString: queryString };
-		return axios.post('/api/auth/search',  query );
+		return axios.post('/API/auth/search',  query );
 	},
 	wildcardSearch: (organizationID, queryString) => {
 		const query = { organizationID: organizationID, queryString: queryString };
-		return axios.post('/api/auth/wildcard', query );
+		return axios.post('/API/auth/wildcard', query );
 	},
 	test: (newEmployee, cb) => {
 		cb(newEmployee);
